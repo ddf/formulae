cf5d773557942d6f42c0a3bf5cdfb1c25d302380
Damien Quartz <dcomposer@gmail.com>
2019-02-06 19:23:53 -0600

    replaced the OF icon with our icon and applied icon to app window on launch
    + needed to replace int main() entry point with WinMain in order to set the icon on the app window at launch, otherwise it shows the generic console application icon

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,25 @@
 #include "ofApp.h"
 
 //========================================================================
-int main( ){
+// for why Windows is different, see: https://forum.openframeworks.cc/t/how-to-get-rid-of-console-and-set-proper-app-icon/24227
+
+#ifdef WIN32
+#include "../resource.h"
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+#else
+int main( )
+#endif
+{
 	ofSetupOpenGL(1920,1080,OF_WINDOW);			// <-------- setup the GL context
+
+#ifdef WIN32
+	HWND hwnd = ofGetWin32Window();
+	HICON hMyIcon = LoadIcon(hInstance, MAKEINTRESOURCE(MAIN_ICON));
+	SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)hMyIcon);
+#endif
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
 	ofRunApp(new ofApp());
-
 }
